04ac9f364628b22f98f4ae21add5613ec3b92474
jamespantalones <jamespants@gmail.com>
2022-04-30 18:55:17 -0400

    fix again

--- components/Foot/Foot.tsx
+++ components/Foot/Foot.tsx
@@ -6,7 +6,7 @@ export function Foot() {
   const { state, methods } = useAudioContext();
 
   return (
-    <footer className="px-2 py-0 bg-neutral-100 relative border-t-2 border-black right-0">
+    <footer className="px-2 py-0 bg-neutral-100 fixed bottom-0 left-0 right-0 border-t-2 border-b-2 border-black right-0">
       <TempoSlider bpm={state.bpm} onChange={methods.changeBpm} />
     </footer>
   );
